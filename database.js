8906c7550c3ee243c93d7dd215c0daa8fae7e859
Ali Nawaz <131615374+Ali01129@users.noreply.github.com>
2024-08-29 20:27:15 +0500

    first commit

---
+++ database.js
@@ -0,0 +1,15 @@
+const { Client } = require('pg');
+
+const client = new Client({
+    host: 'localhost',
+    user: 'postgres',
+    password: 'ali',
+    port: 5432,
+    database: 'ritter_dahait',
+});
+
+client.connect()
+    .then(() => console.log('Connected to Postgres database'))
+    .catch(err => console.error('Error connecting to database', err));
+
+module.exports = client;
