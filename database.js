d482184e9238587fe28e33e46cdc023618c67346
Ali Nawaz <131615374+Ali01129@users.noreply.github.com>
2024-09-18 14:09:07 +0500

    database issue

--- database.js
+++ database.js
@@ -1,19 +1,12 @@
 const { Client } = require('pg');
 require('dotenv').config();
 
-const host=process.env.HOST;
-const user=process.env.USER;
-const password=process.env.PASSWORD;
-const port=process.env.PORT;
-const database=process.env.DATABASE;
-
-
 const client = new Client({
-    host: host,
-    user: user,
-    password: password,
-    port: port,
-    database: database,
+    host: 'localhost',
+    user: 'postgres',
+    password: 'ali',
+    port: 5432,
+    database: 'ritter_dahait',
 });
 
 client.connect()
